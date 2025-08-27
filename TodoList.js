f4860f28fbaff37d2e1b445080bd249fe44beb1d
Aksprofessional <aksprofessional1@gmail.com>
2025-08-27 05:54:41 -0700

    Update TodoList.js

--- TodoList.js
+++ TodoList.js
@@ -5,25 +5,44 @@ function TodoList() {
   const [todoList, setTodoList] = useState([]);
 
   const addTodo = () => {
+    if (input === "") return;
     const item = {
-      id:0,
+      id:todoList.length+1,
       text:input,
       completed:false
     }
     
-    setTodoList(prev=>[...prev,item])
+    setTodoList(prev => [...prev, item])
+    setInput("")
+  }
+
+  const toggleCompleted = (id) => {
+    setTodoList(todoList.map((t) => {
+      if (t.id === id) {
+        return {
+          ...t,
+          completed: !t.completed
+        }
+      }
+      else
+        return t;
+    }))
+  }
+
+  const deleteTodo=(id) => {
+    setTodoList(todoList.filter((t)=>t.id!==id))
   }
 
     return (
       <div>
         <input type='text' placeholder='Enter todo' value={input} onChange={(e)=>setInput(e.target.value)}></input>
-        <button onClick={(t)=>addTodo()}>Add</button>
+        <button onClick={()=>addTodo()}>Add</button>
         <ul>
-          {todoList.map(t =>
+          {todoList.map((t) =>
             <li key={t.id}>
-              <input type='checkbox'/>
-              <span>{t.text}</span>
-             <button>Delete</button>
+              <input type='checkbox' checked={t.completed} onChange={()=>toggleCompleted(t.id)}/>
+              <span className={t.completed ? "strikeThrough" : ""}>{t.text}</span>
+             <button onClick={()=>deleteTodo(t.id)}>Delete</button>
             </li>)}
         </ul>
       </div>
